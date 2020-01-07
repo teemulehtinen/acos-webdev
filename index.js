24231d3509fd069e54ac8207a7df7328b8a89c12
Lehtinen Teemu <lehtint6@t31300-lr146.org.aalto.fi>
2020-01-07 09:12:50 +0200

    Refactor common templates to content type

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-/* global module, require */
+/* global module, require, __dirname, console */
 /* jshint globalstrict: true */
 'use strict';
 
 var fs = require('fs');
+let nj = require('nunjucks');
+let baseDir = __dirname;
 
 var Type = function () {};
 Type.meta = {
@@ -28,43 +30,49 @@ Type.register = function (handlers, app, conf) {
     }
 };
 
-Type.addToHead = function (params) {
-  return '<link href="/static/webdev/acos-webdev.css" rel="stylesheet">\n'
-    + '<script src="/static/webdev/acos-webdev.js" type="text/javascript"></script>\n';
-};
-
-Type.addToBody = function (params) {
-    return '';
-};
-
 Type.uniqueUserID = function (req) {
   /* Try the known protocol values or fallback to zero. */
   return parseInt((req.body.user_id || req.query.uid || '0').match(/\d+/g).join(''))
 }
 
 Type.initialize = function (req, params, handlers, cb) {
+  let templateDir = baseDir + '/templates/';
+  nj.configure(templateDir, { autoescape: false });
 
   // Select AB-test population
-  params.abFlag = Type.uniqueUserID(req) % 2 == 1;
-
-  // Initialize the content type
-  params.headContent += Type.addToHead(params);
-  params.bodyContent += Type.addToBody(params);
+  let abFlag = Type.uniqueUserID(req) % 2 == 1;
 
   // Initialize the content package
-  handlers.contentPackages[req.params.contentPackage].initialize(req, params, handlers, function () {
+  handlers.contentPackages[req.params.contentPackage].initialize(req, params, handlers, function (config) {
+    config.abFlag = abFlag
+
+    if (config.template) {
+      config.html = nj.render(config.template, config);
+    }
+
+    let templateParam = {
+      id: 'acos-' + req.params.contentPackage + '-' + params.name,
+      class: 'acos-' + req.params.contentType + '-exercise acos-' + req.params.contentPackage,
+      resetButton: config.resetButton,
+      config: JSON.stringify(config),
+      script: typeof(config.script) == 'function' ? config.script.toString() : undefined,
+      points: typeof(config.points) == 'function' ? config.points.toString() : undefined
+    };
+    params.headContent += nj.render('head.html', templateParam);
+    params.bodyContent += nj.render('body.html', templateParam);
+
     cb();
   });
 };
 
 Type.handleEvent = function (event, payload, req, res, protocolPayload, responseObj, cb) {
-  var dir = Type.logDirectory + req.params.contentPackage;
-  if (event == 'log') {
+  if (event == 'log' || event == 'grade') {
+    var dir = Type.logDirectory + req.params.contentPackage;
     fs.mkdir(dir, '0775¨', function (err) {
       var data = new Date().toISOString()
         + '\t' + JSON.stringify(payload.log)
         + '\t' + JSON.stringify(protocolPayload || {}) + '\n';
-      var logName = payload.problemName.replace(/\.|\/|\\|~/g, "-") + '.log';
+      var logName = req.params.name.replace(/\.|\/|\\|~/g, "-") + '.log';
       fs.writeFile(dir + '/' + logName, data, {flag: 'a'}, function (err) {
         cb(event, payload, req, res, protocolPayload, responseObj);
       });
